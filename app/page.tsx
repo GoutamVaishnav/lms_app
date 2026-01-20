4b8f10fd9de5532e0eca2a700d057a858952c559
GoutamVaishnav <goutamvaishnav09@gmail.com>
2026-01-20 19:09:55 +0530

    Home Page UI completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,53 @@
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
 import { Button } from "@/components/ui/button";
+import { recentSessions } from "@/constants";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1></h1>
-      <Button>start</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="science"
+          duration={30}
+          color="#e5d0ff"
+        />
+
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="science"
+          duration={30}
+          color="#e5d0ff"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA/> 
+      </section>
+    </main>
   );
 };
 
